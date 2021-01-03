9a7905faeb75706ab747ecd62cdd9cb92f1994f7
Jim Geist <info@jimgeist.com>
2021-01-02 21:39:59 -0500

    Ver1: Creation of Jasmine tests to test the Tip Pool application.

---
+++ README.md
@@ -0,0 +1,17 @@
+# sb_07-01-12_JasmineExercises
+
+## Part 2 - Further Study: Tip Pool
+
+Please go to [Tip Pool Jasmine Tests](https://jimgeist.github.io/sb_07-01-12-2_JasmineExercises-TipPool/) for the html of the tip pool application with the Jasmine Tests.
+
+## Assignment Details
+
+# Part Two - Tip Pool
+First part of the assignment involved completing server.test.js by adding in the necessary clean up functions. 
+The second part, which ate up tooooo much time, was to test the remainder of the application. There were many starts, stops, and restarts on this one. I just was not expecting the amount of object rebuilds in order to test all the functions. It was interesting to see how the code was broken up into 3 JavaScript files depending on whether it was Server-related (Waiter would have been a better term) or Payment-related functions. 
+
+
+# Part One - Loan Calculator
+Assignment involved creating Jasmine tests to test the calculateMonthlyPayment function. 
+
+Please go to github repository [Tip Pool Jasmine Tests](https://github.com/JimGeist/sb_07-01-12-1_JasmineExercises-LoanCalc/) for the ReadMe with the details for the Loan Calculator test portion of this assignment.
---
+++ servers.test.js
@@ -0,0 +1,54 @@
+describe("SERVER.JS FUNCTION TESTS", function () {
+
+  describe("submitServerInfo tests", function () {
+
+    beforeEach(function () {
+      // initialization logic
+      serverNameInput.value = 'Alice';
+      submitServerInfo();
+    });
+
+    it('A new server should exist in allServers from the setup submitServerInfo()', function () {
+      expect(Object.keys(allServers).length).toEqual(1);
+      expect(allServers['server' + serverId].serverName).toEqual('Alice');
+    });
+
+    it('The Server Name / Earnings table in the UI should include the new server', function () {
+      expect(serverTbody.innerHTML).toEqual('<tr id="server1"><td>Alice</td><td>$0.00</td></tr>');
+    });
+
+    afterEach(function () {
+      // teardown logic
+      serverTbody.innerHTML = '';
+      delete allServers["server1"];
+      serverId = 0;
+    });
+
+  })
+
+  describe("updateServerTable tests when there are no bills", function () {
+
+    beforeEach(function () {
+      // initialization logic
+      allServers.server1 = { serverName: 'Ashley' };
+      allServers.server2 = { serverName: 'Rafaela' };
+      updateServerTable();
+    });
+
+    it('Passes when two servers exist in the Server Name/ Earnings table in the UI with $0 earnings', function () {
+      expect(serverTbody.innerHTML).toContain('<tr id="server1"><td>Ashley</td><td>$0.00</td></tr>');
+      expect(serverTbody.innerHTML).toContain('<tr id="server2"><td>Rafaela</td><td>$0.00</td></tr>');
+    });
+
+
+    afterEach(function () {
+      // teardown logic
+      serverTbody.innerHTML = '';
+      delete allServers["server1"];
+      delete allServers["server2"];
+    });
+
+  });
+
+
+});
